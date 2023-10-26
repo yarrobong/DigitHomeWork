1c187fccc47be80a217bbd592f8ef25cd5469e0b
yarrobong <oneboardsholi@gmail.com>
2023-10-26 10:39:07 +0500

    Добавьте файлы проекта.

---
+++ DigitHomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿string str = Console.ReadLine();
+string result = String.Empty;
+foreach (char item in str)
+{
+    if(Char.IsDigit(item))
+    {
+        if (!result.Contains(item))
+        {
+            result = result + item;
+        }
+    }
+}
+if (result == String.Empty)
+{
+    result = "NO";
+}
+Console.WriteLine(result);
